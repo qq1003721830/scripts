23f7814fc03ebb32e314de154c9c9e50fe8a264e
Jack wang <1003721830@qq.com>
2019-06-15 08:32:43 +0800

    tes3

--- flashoffers.py
+++ flashoffers.py
@@ -106,103 +106,19 @@ def zhuoping():
 
 
 
-# for n in range(24):
-#     print(n)
-#     zhilian()
-#     liepin()
-#     qiancheng()
-#     time.sleep(600)
-
-
-#################################################################################################
-
-
-site = "adb shell am start -n com.zhaopin.social/.SplashActivity"
-position = ["运营总监", "产品总监", "客服"]
-region_key = ["xiamen", "shanghai", "guangzhou", "shenzheng",
-              "hangzhou"]  # "xiamen"+2,"shanghai"+0,"guangzhou"+0,"shenzheng"+0,"hangzhou"+1
-
-
-
-def delivery(site, position, n):
-    region_value = ["516 1616", "528 731", "510 908", "523 1104", "534 1696"]
-    os.system(site)
-    time.sleep(5)
-
-    os.system("adb shell  input tap 430 160")  # 选择地区
-    time.sleep(3)
-
-    os.system("adb shell  input tap 99 130")  # 点击选择
-    time.sleep(3)
-
-
-
-    if region_key[n] == "shanghai" or region_key[n] == "guangzhou" or region_key[n] == "shenzheng":
-        print("this is :" + region_key[n])
-        os.system("adb shell  input tap " + region_value[n])
-        time.sleep(3)
+#for n in range(24):
+n=0
+while n<300 :
+    print(n)
+    n+=1
+    zhilian()
+    liepin()
+    qiancheng()
+    time.sleep(120)
 
 
 
 
-    elif region_key[n] == "hangzhou":
-        print("this is :" + region_key[n])
-        for a in range(1):
-            os.system("adb shell input swipe 550 1740 550 750")  # 翻1页
-            time.sleep(1)
-        os.system("adb shell  input tap " + region_value[n])
-        time.sleep(1)
 
-
-
-
-    elif region_key[n] == "xiamen":
-        print("this is: " + region_key[n])
-        for a in range(2):
-            os.system("adb shell input swipe 550 1740 550 750")  # 翻2页
-            time.sleep(1)
-        os.system("adb shell  input tap " + region_value[n])
-        time.sleep(1)
-
-
-
-
-    text = position  # 搜索
-    os.system("adb shell am broadcast -a ADB_INPUT_TEXT --es msg '" + text + "'")
-    os.system("adb shell input keyevent 66")
-
-
-
-    os.system("adb shell  input tap 380 300")  # 筛选
-    time.sleep(1)
-    os.system("adb shell input swipe 105 735 675 695")
-    time.sleep(1)
-    os.system("adb shell  input tap 545 1536")
-
-
-
-
-    os.system("adb shell  input tap 970 150")  # 批量投递
-    x = 3
-    for n in range(x):
-        os.system("adb shell  input tap 90 1861")
-        time.sleep(1)
-        os.system("adb shell  input tap 860 1848")
-        time.sleep(1)
-        os.system("adb shell  input tap 90 1861")
-        time.sleep(1)
-        for n in range(12):
-            os.system("adb shell input swipe 550 1740 550 290")
-            time.sleep(1)
-        time.sleep(2)
-
-    os.system("adb shell am force-stop com.zhaopin.social")  # 关闭程序
-    print("exe delivery Success:", position,region_key[n-1], site)
-
-
-
-
-if __name__ == '__main__':
-    delivery(site, position[0], 3)
 
 
